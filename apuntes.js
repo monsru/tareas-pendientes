1807920b2ecdea07a99d52ee765ab95c96e6bf90
monsru <150310072+monsru@users.noreply.github.com>
2024-08-07 20:48:11 -0600

    Add files via upload

---
+++ apuntes.js
@@ -0,0 +1,53 @@
+// Comemetarios 
+// alt + 39 ''
+
+// alert('Hola desde un alert');
+
+// var apellido = "Rojas";
+let nombre = "Regina";
+
+const pi = 3.14;
+// string nombre ="Juan";
+// Tipos de datos
+
+// No definido
+let noDefinido;
+// Número
+let cantidad = 50;
+// Cadena de texto o String
+let bebida = "agua";
+// booleanos
+let activo = true;
+
+// Objetos
+// Persona: nombre, apellido, edad, peso 
+let persona = {nombre : 'Aurora', apellido : 'Pinzón', edad : 20,};
+console.log(persona);
+// Arrays colección 
+let personas = [
+    {nombre: 'Melaanie', apellido : 'Matus', edad : 21},
+    {nombre: 'Mayra', apellido : 'Arias', edad : 19},
+    {nombre: 'Karla', apellido : 'Yulenni', edad : 22},
+    persona
+]
+console.log(personas);
+console.log(personas[2]);
+
+// Funciones JS
+// Listado de instrucciones
+function suma(a, b) {
+    return a + b;
+}
+let resultadoDeSuma = suma(9, 7);
+console.log(resultadoDeSuma);
+
+function multiplicacion(a, b) {
+    return a * b;
+}
+let resultado = multiplicacion(5,7);
+console.log(resultado);
+
+
+
+
+
